548b24f682deffb27f1136f7120f8d81cb131c1d
sasha2008e <124662749+sasha2008e@users.noreply.github.com>
2025-10-05 14:46:24 +0300

    Update 3a-t4.cpp
    I used if statement to modify the program so that it outputs a warning if the user input is not an integer.

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,31 +1,27 @@
 #include<iostream>
+#include<limits>
 using namespace std;
-int main()
-{ 
+Bint main()
 int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin > › k;
-日
-if (k == 0)
+cout « " Enter the amount of integers to sum = ; cin »> k;
+while (1)
 {
-	cout << "k can not be equal to 0!";
-
-else {
-	do {
-		cout « < "Enter integer nr. " « i «": ";
-		cin > › number;
-		sum_k += number; i++;
-	} while (i <= k);
-	cout << " The total sum of " « k «" integers is: " « sum_k;
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "You have entered wrong input << endl; cout « " Enter the amount of integers to sum = "; 
+			cin >> k;
+	}
+	if (!cin.fail())
+		break;
 }
-return 0;
-
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
 do {
-cout << "Enter integer nr. " << i << ": ";
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
+	cout « "Enter integer nr." « < i «": "; cin > › number;
+	sum_k += number; i++;
+}while (i <= k);
+cout « " The total sum of " « k«" integers is: " « sum_k;
 return 0;
+
 }
+
